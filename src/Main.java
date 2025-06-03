5c433c3915f05fe5e9edd7aef1ad60e3933946fb
Thanos <salabasisthan75@gmail.com>
2025-06-03 20:21:33 +0300

    2ο

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> new GUI().setVisible(true));
-		String file = "./Sxoles/thetiki_cleaned.csv";
-
-		fileScanner scanner = new fileScanner();
-		ArrayList<School> schools = scanner.getSchools(file);
-
 	}
 
 }
