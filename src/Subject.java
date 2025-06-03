ba505cdff5ea1f300903a9fc54224c544b9a4792
Thanos <salabasisthan75@gmail.com>
2025-06-03 20:18:08 +0300

    2ο

--- src/Subject.java
+++ src/Subject.java
@@ -2,27 +2,4 @@ public class Subject {
     private Integer Grade;
     private Double Weight;
 
-    public Subject(Double Weight) {
-        this.Weight = Weight;
-    }
-
-    public Integer getGrade() {
-        return Grade;
-    }
-
-    public void setGrade(Integer grade) {
-        Grade = grade;
-    }
-
-    public Double getWeight() {
-        return Weight;
-    }
-
-    public void setWeight(Double weight) {
-        Weight = weight;
-    }
-
-    public Double CalculateScore(Integer Grade, Double Weight){
-        return Grade.doubleValue() * (Weight * 100) * 10;
-    }
 }
